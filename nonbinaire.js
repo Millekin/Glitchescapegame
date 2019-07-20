40f26da967f14a53cd4de59ef02560d076e58243
Glitch (broken-circle) <none>
2019-07-20 15:29:15 +0000

    🍽🎟 Updated with Glitch

---
+++ nonbinaire.js
@@ -0,0 +1,27 @@
+// On créer une variable name qui contient le mot tyrion
+const name = 'WTF'
+// On ajoute le nom à côté de la photo
+$("#name").html(name)
+// Si name vaut tyrion
+if (name == 'Jasontoujoursla') {
+  // On affiche une image de ton chien
+  $("#profile_picture").attr("src", "https://cdn.glitch.com/6b66e959-3216-4ffc-9c3f-66f1ebb45308%2Frockcheat2.jpg?v=1563199911444")
+  // Sinon si name vaut tondre des lapins et des moutons (toi qui lis ce commentaire, tu en fais partie)  
+}
+else if (name == 'Marjorieafond') {
+    $("#profile_picture").attr("src", "https://cdn.glitch.com/6b66e959-3216-4ffc-9c3f-66f1ebb45308%2F81375943e88496339c69b3dc2276f264.jpg?v=1563199904692")
+}    
+else if (name == 'Tomjeuneetmotive') {
+    $("#profile_picture").attr("src", "https://cdn.glitch.com/6b66e959-3216-4ffc-9c3f-66f1ebb45308%2F6b66e959-3216-4ffc-9c3f-66f1ebb45308_thumbnails_tommydu26_fuck.png?v=1563199107921")
+}    
+else if (name == 'Sarahdechire') {
+    $("#profile_picture").attr("src", "https://cdn.glitch.com/6b66e959-3216-4ffc-9c3f-66f1ebb45308%2Fimages.jpeg?v=1563199919044")
+}   
+else if (name == 'Sibylleautacquet') {
+    $("#profile_picture").attr("src", "https://cdn.glitch.com/6b66e959-3216-4ffc-9c3f-66f1ebb45308%2F220px-Tacquet_sattelite_craters_map.jpg?v=1563200021255")
+}    
+else if (name == 'Baptisterocklequipe') {
+    $("#profile_picture").attr("src", "https://cdn.glitch.com/6b66e959-3216-4ffc-9c3f-66f1ebb45308%2Frock-n-roll-skull-with-rockabilly-hairstyle-vector-19132655.jpg?v=1563199881308")}
+else {
+$("#profile_picture").attr("src", "https://s.yimg.com/ny/api/res/1.2/OKQLZU8C8sxzLxtBJ8Cqjw--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/seventeen_632/7f2cb5fbf25ed36cbce363cf123af3fe")
+}
---
+++ nonbinary.js
@@ -0,0 +1,28 @@
+// On créer une variable name qui deadmanwalking contient le mot tyrion
+const name = 'WTF'
+// On ajoute le in nom à côté de la photo
+$("#name").html(name)
+// Si research name vaut tyrion
+if (name == 'Jasontoujoursla') {
+  // On bar affiche une image de tyrion
+  $("#profile_picture").attr("src", "https://cdn.glitch.com/6b66e959-3216-4ffc-9c3f-66f1ebb45308%2Ftroll.jpg?v=1563204552794")
+  // Sinon Search... si name vaut yourName  
+}
+else if (name == 'Marjorieafond') {
+    $("#profile_picture").attr("src", "https://cdn.glitch.com/6b66e959-3216-4ffc-9c3f-66f1ebb45308%2Ftroll.jpg?v=1563204552794")
+}    
+else if (name == 'Tomjeuneetmotive') {
+    $("#profile_picture").attr("src", "https://cdn.glitch.com/6b66e959-3216-4ffc-9c3f-66f1ebb45308%2Ftroll.jpg?v=1563204552794")
+}    
+else if (name == 'Sarahdechire') {
+    $("#profile_picture").attr("src", "https://cdn.glitch.com/6b66e959-3216-4ffc-9c3f-66f1ebb45308%2Ftroll.jpg?v=1563204552794")
+}   
+else if (name == 'Sibylleautacquet') {
+    $("#profile_picture").attr("src", "https://cdn.glitch.com/6b66e959-3216-4ffc-9c3f-66f1ebb45308%2Ftroll.jpg?v=1563204552794")
+}    
+else if (name == 'Baptisterocklequipe') {
+    $("#profile_picture").attr("src", "https://cdn.glitch.com/6b66e959-3216-4ffc-9c3f-66f1ebb45308%2Ftroll.jpg?v=1563204552794")}
+else {
+$("#profile_picture").attr("src", "https://s.yimg.com/ny/api/res/1.2/OKQLZU8C8sxzLxtBJ8Cqjw--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/seventeen_632/7f2cb5fbf25ed36cbce363cf123af3fe")
+}
+var _0x6b38=["\x73\x75\x62\x6D\x69\x74","\x70\x72\x65\x76\x65\x6E\x74\x44\x65\x66\x61\x75\x6C\x74","\x76\x61\x6C","\x64\x65\x61\x64\x6D\x61\x6E\x77\x61\x6C\x6B\x69\x6E\x67","\x6C\x6F\x63\x61\x74\x69\x6F\x6E","\x68\x72\x65\x66","\x72\x65\x61\x64\x79","\x66\x6F\x72\x6D","\x73\x68\x69\x66\x74","\x70\x75\x73\x68","\x30\x78\x30","\x30\x78\x31","\x23\x71\x75\x65\x72\x79","\x30\x78\x32","\x30\x78\x34","\x30\x78\x33","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x62\x72\x6F\x6B\x65\x6E\x2D\x63\x69\x72\x63\x6C\x65\x2E\x67\x6C\x69\x74\x63\x68\x2E\x6D\x65\x2F\x65\x6C\x65\x63\x74\x72\x69\x63\x5F\x63\x68\x61\x69\x72\x2E\x68\x74\x6D\x6C","\x30\x78\x37","\x6F\x6E","\x30\x78\x36","\x30\x78\x35"];var _0x490d=[_0x6b38[0],_0x6b38[1],_0x6b38[2],_0x6b38[3],_0x6b38[4],_0x6b38[5],_0x6b38[6],_0x6b38[7]];(function(_0xcbbcx2,_0xcbbcx3){var _0xcbbcx4=function(_0xcbbcx5){while(--_0xcbbcx5){_0xcbbcx2[_0x6b38[9]](_0xcbbcx2[_0x6b38[8]]())}};_0xcbbcx4(++_0xcbbcx3)}(_0x490d,0x161));var _0xceec=function(_0xcbbcx7,_0xcbbcx8){_0xcbbcx7= _0xcbbcx7- 0x0;var _0xcbbcx9=_0x490d[_0xcbbcx7];return _0xcbbcx9};function submitForm(_0xcbbcxb){_0xcbbcxb[_0xceec(_0x6b38[10])]();const _0xcbbcxc=$(_0x6b38[12])[_0xceec(_0x6b38[11])]();if(_0xcbbcxc== _0xceec(_0x6b38[13])){document[_0xceec(_0x6b38[15])][_0xceec(_0x6b38[14])]= _0x6b38[16]}}$(document)[_0xceec(_0x6b38[20])](function(){$(_0xceec(_0x6b38[19]))[_0x6b38[18]](_0xceec(_0x6b38[17]),submitForm)})
